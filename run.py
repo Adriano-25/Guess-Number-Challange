17778b50162b459feadfa6a38a7d9aa2a7b08b34
Adriano-25 <>
2023-08-11 23:32:06 +0000

    Add to  define any data structure what we will go thru the game

--- run.py
+++ run.py
@@ -1,3 +1,13 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+import random
+import time
+
+# Dictionary to store player scores (name: score)
+leaderboard = {}
+
+# Difficulty levels with their corresponding range and time limit
+difficulty_levels = {
+    "easy": {"range": (1, 10), "time_limit": 15},
+    "medium": {"range": (1, 20), "time_limit": 10},
+    "hard": {"range": (1, 50), "time_limit": 7}
+}
+print(difficulty_levels)
