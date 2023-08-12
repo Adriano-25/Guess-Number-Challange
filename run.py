4d246644f225a082cff3069f8a60f4bc5c9ff184
Adriano-25 <>
2023-08-12 06:45:12 +0000

    Added leaderboard to the game

--- run.py
+++ run.py
@@ -11,6 +11,9 @@ print(f"Welcome, {player_name}!")
 # Set the maximum number of attempts
 max_attempts = 5
 
+# Initialize leaderboard
+leaderboard = {}
+
 while True:  # Keep the game running until the player decides to stop
     # Generate a random secret number between 1 and 100
     secret_number = random.randint(1, 100)
@@ -26,7 +29,7 @@ while True:  # Keep the game running until the player decides to stop
             difference = abs(guess - secret_number)
 
             if guess == secret_number:
-                print(f"Congratulations, {player_name}! You guessed !{secret_number}!")
+                print(f"Congratulations, {player_name}! You guessed the secret number {secret_number}!")
                 break
             elif difference <= 10:
                 print("Warm!")
@@ -42,6 +45,17 @@ while True:  # Keep the game running until the player decides to stop
     if attempts == max_attempts:
         print(f"Sorry, {player_name}. You've reached the maximum number of attempts.")
         print(f"The secret number was {secret_number}. Better luck next time!")
+
+    # Update the leaderboard
+    leaderboard[player_name] = max_attempts - attempts
+
+    # Ask the player if they want to view the leaderboard
+    view_leaderboard = input("Do you want to view the leaderboard? (yes/no): ").lower()
+    if view_leaderboard == "yes":
+        print("\nLeaderboard:")
+        sorted_leaderboard = sorted(leaderboard.items(), key=lambda x: x[1], reverse=True)
+        for name, score in sorted_leaderboard:
+            print(f"{name}: {score} attempts")
 
     # Ask the player if they want to play again
     play_again = input("Do you want to play again? (yes/no): ").lower()
