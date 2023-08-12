cd98030e472edac48731eac4bb8d256b705be1d9
Adriano-25 <>
2023-08-12 00:30:08 +0000

    desplay a secret number for testing

--- run.py
+++ run.py
@@ -1,5 +1,15 @@
 import random
 
-#Welcome message
+
+# Welcome message
 print("Welcome to the Guess Number Challange!")
 print("Can you guess the secret number?")
+
+# Ask for the player's name
+player_name = input("What is your name? ")
+
+# Generate a random secret number beetween 0 and 100
+secret_number = random.randint(1, 100)
+
+# Desplay a secret number for testing purpose
+print(f"DEBUG: Secret number is {secret_number}")
