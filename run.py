17d8e0d69766715116b7eff7963848c9512c37c0
Adriano-25 <>
2023-08-12 05:47:02 +0000

    Ask the player for a guess

--- run.py
+++ run.py
@@ -13,3 +13,24 @@ secret_number = random.randint(1, 100)
 
 # Desplay a secret number for testing purpose
 print(f"DEBUG: Secret number is {secret_number}")
+
+# Set the maximum number of attempts
+max_attempts = 5
+attempts = 0
+
+# Set the maximum number of attempts
+max_attempts = 5
+attempts = 0
+
+# Main game loop
+while attempts < max_attempts:
+    # Ask the player for a guess
+    guess = int(input("Take a guess: "))
+# Set the maximum number of attempts
+max_attempts = 5
+attempts = 0
+
+# Main game loop
+while attempts < max_attempts:
+    # Ask the player for a guess
+    guess = int(input("Take a guess: "))
