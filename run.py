9eed7bbbf5312f27ad6c6298c08e327aba21149d
Adriano-25 <>
2023-08-12 00:12:57 +0000

    Welcome message

--- run.py
+++ run.py
@@ -1,13 +1,5 @@
 import random
-import time
 
-# Dictionary to store player scores (name: score)
-leaderboard = {}
-
-# Difficulty levels with their corresponding range and time limit
-difficulty_levels = {
-    "easy": {"range": (1, 10), "time_limit": 15},
-    "medium": {"range": (1, 20), "time_limit": 10},
-    "hard": {"range": (1, 50), "time_limit": 7}
-}
-print(difficulty_levels)
+#Welcome message
+print("Welcome to the Guess Number Challange!")
+print("Can you guess the secret number?")
